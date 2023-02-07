00a1d26248a627fb9be536704ccfa56546a1e0af
Seweryn Woźniak <sewerynwozniak111$gmail.com>
2023-02-07 11:36:06 +0100

    adding routes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { NavLink} from 'react-router-dom';
+
+ 
+
+
+
+const Navbar = () => {
+  return (
+    <div>
+        <li>
+            <NavLink to='/'>
+                Home
+            </NavLink>
+        </li>
+        <li>
+            <NavLink to='/music'>
+                Music
+            </NavLink>
+        </li>
+        <li>
+            <NavLink to='/artist'>
+                Artist
+            </NavLink>
+        </li>
+        <li>
+            <NavLink to='/signup'>
+                Sign up
+            </NavLink>
+        </li>
+        
+      
+
+
+
+
+
+    </div>
+
+    
+  )
+}
+
+export default Navbar
