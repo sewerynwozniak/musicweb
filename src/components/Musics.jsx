b87a76804db0cde78947dc3ecc410c44b81226b1
Seweryn Woźniak <sewerynwozniak111$gmail.com>
2023-04-14 00:05:41 +0200

    adding selectingStar function

--- src/components/Musics.jsx
+++ src/components/Musics.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import {db} from '../firebase'
-import { collection, getDocs  } from 'firebase/firestore/lite';
+import { collection, getDocs, updateDoc, doc } from 'firebase/firestore/lite';
 import AddMusic from './AddMusic';
 import starGrey from '../../src/assets/star-grey.png'
 import starGold from '../../src/assets/star-gold.png'
@@ -32,7 +32,7 @@ const Musics = () => {
   let starsArray = Array.from({length: 10}, (_, i) => i + 1);
 
 
-  const selectingStars = (e)=>{
+  const highlightingStars = (e)=>{
 
     const starIndex = e.target.dataset.index
     const parent = e.target.parentElement
@@ -41,14 +41,46 @@ const Musics = () => {
     selectedStarsArray.forEach(el=>el.src=starGold)
 
   }
-  const unselectingStars = (e)=>{
+  const unhighlightingStars = (e)=>{
 
     const parent = e.target.parentElement
     const selectedStarsArray = Array.from(parent.children);
     selectedStarsArray.forEach(el=>el.src=starGrey)
 
   }
+  const selectingStar = async (e)=>{
 
+    const songId = e.target.parentElement.parentElement.dataset.id;
+    const starIndex = Number(e.target.dataset.index);
+    await updateDoc(doc(db, 'Song', songId), {rating: starIndex});
+
+  }
+
+
+
+  let songsArray = songs.map(song=>(
+    <li 
+        data-id={song.id}
+        href=""
+      >
+        
+
+      {song.name}      
+      <div className="musics__starsWrapper">
+        {starsArray.map(star=>(
+          <img 
+            onMouseOver={e=>highlightingStars(e)}
+            onMouseLeave={e=>unhighlightingStars(e)}
+            onClick={e=>selectingStar(e)}
+            className='musics__star' 
+            data-index={star} 
+            src={starGrey} alt="" 
+          />
+        ))}
+      </div>
+    
+    </li>
+  ))
   
   return (
     
@@ -59,32 +91,13 @@ const Musics = () => {
         <p className='musics__headline'>List</p>
     
         <ul className='musics__ul'> 
-          {songs.map(song=>{
-           return(
-
-            <li 
-             
-              href="">
-
-              {song.name}      
-              <div className="musics__starsWrapper">
-                {starsArray.map(star=>(
-                  <img 
-                    onMouseOver={e => selectingStars(e)}
-                    onMouseLeave={e => unselectingStars(e)}
-           
-                    className='musics__star' 
-                    data-index={star} 
-                    src={starGrey} alt="" 
-                  />
-                ))}
-              </div>
-              
-            </li>
-
-           )
-          })}
+          {songsArray}
         </ul> 
+
+
+
+
+
      </div>
   )
 }
